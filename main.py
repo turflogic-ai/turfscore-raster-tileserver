7b9a465c0ecf76ac16e161f4efa3e36f0ab8af05
kojo <senav103@gmail.com>
2025-10-07 23:55:37 -0500

    add proxy pass to nginx conf

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
 sentry_dsn = os.getenv("SENTRY_DSN")
 if sentry_dsn:
     sentry_sdk.init(
-        dsn="https://9e0948d636e9a5afabb4fd9f87d21e11@o4509627532640256.ingest.us.sentry.io/4510151900266496",
+        dsn=sentry_dsn,
         # Add data like request headers and IP for users,
         # see https://docs.sentry.io/platforms/python/data-management/data-collected/ for more info
         traces_sample_rate=0.1,
